f55eb49f12c3a9a68b45ab8237a695db21a681ed
wxa <wxa1397309529@163.com>
2020-05-04 03:16:50 +0800

    新建wxa分支

---
+++ src/components/common/IndexHeader.jsx
@@ -0,0 +1,37 @@
+import React, { Component } from 'react'
+
+const h3Style = {
+    "font-size":"0.48rem",
+    "line-height":"0.61333rem",
+    "height":"0.61333rem",
+    "color":"#232323",
+    "font-weight":700
+}
+export default class IndexHeader extends Component {
+    render() {
+        return (
+            <div style={{
+                "display":"flex",
+                "-webkit-box-pack":"justify",
+                "justify-content":"space-between"
+            }}>
+                <h3 style={h3Style}>{this.props.title}</h3>
+                <div style={{
+                    "display":"flex",
+                    "-webkit-box-align":"center",
+                    "align-items":"center",
+                    "padding-right":"0.13333rem"
+                }}>
+                    <span style={{
+                        "font-size":"0.34667rem",
+                        "color":"#232323"
+                    }}>全部</span>
+                    <img style={{
+                        width:"0.53333rem",
+                        height:"0.53333rem"
+                    }} src="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAACgAAAAoBAMAAAB+0KVeAAAALVBMVEUAAABmZmZmZmZmZmZmZmZmZmZmZmZmZmZmZmZmZmZmZmZmZmZmZmZmZmZmZmaTgChpAAAAD3RSTlMA/Aa1qrzcwa+ezcijk5JAYXVXAAAAVUlEQVQoz2MYtoDNAIsghzA2pYEOWASZRbApNWzAIqiKTSlT4QEsouqS2JQGXsAiqpVApCDTxgeYgtoy2OyZQJyLmAqxhAirBLbwcCAy5NgaGIYzAADnxQppL/x7ngAAAABJRU5ErkJggg==" alt=""/>
+                </div>
+            </div>
+        )
+    }
+}
