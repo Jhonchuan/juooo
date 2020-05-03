6558be12b1793463aabd01c361043d80382d105a
liushuo <2519067170@qq.com>
2020-05-03 23:16:20 +0800

    123123

--- src/views/Show.jsx
+++ src/views/Show.jsx
@@ -1,67 +1,44 @@
 import React, {Component} from 'react'
 import "../assets/style/show/index.css"
+import Comm from "../components/common/CommonHeader.jsx"
 
 export default class Show extends Component {
     render() {
         return (
-            <div id="app" className>
-
-                <div className="page show_library">
-                    <section className="title library-title">
-                        <span className="data-v-0f98d9e6">演出</span>
-                        <i className="iconfont ju-icon-arrow title__back"></i>
-                        <div className="title__nav-wrapper ">
-                            <div className="title__nav">
-                                <i className="iconfont ju-icon-ellipsis title__nav__ellipsis"></i>
-                            </div>
-                        </div>
-                    </section>
-                    <div className="show_type_wrap">
-                        <div className="show_type js_li_type">
-                            <ul>
-                                <li className="active">全部</li>
-                                <li className>演唱会</li>
-                                <li className>音乐会</li>
-                                <li className>话剧歌据</li>
-                            </ul>
-                        </div>
-                        <div className="city_wrap">
-                            <span className="city">全国</span>
-                            <span className="city_icon"></span>
-                        </div>
+            <div>
+                {/*<Comm title="演出"></Comm>*/}
+                {/*<img src="https://image.juooo.com/group1/M00/03/25/rAoKmV2f3-yADPouAACAMESBbK8067.jpg" alt=""/>*/}
+                <div className="show_type_wrap">
+                    <div className="show_type js_li_type">
+                        <ul>
+                            <li className="active">全部</li>
+                            <li>演唱会</li>
+                            <li>音乐会</li>
+                            <li>话剧歌剧</li>
+                        </ul>
                     </div>
-                    <div className="library-wrap">
-                        <div>
-                            <div className="show-model">
-                                <div className="show-model__main">
-
+                    <div className="city_wrap">
+                        <span className="city">全国</span>
+                        <span className="city_icon"></span>
+                    </div>
+                </div>
+                <div className="library-wrap">
+                    <div>
+                        <div className="show-model">
+                            <div className="show-model__main">
+                                <div className="show-model__item">
+                                    <img src="https://image.juooo.com/group1/M00/03/25/rAoKmV2f3-yADPouAACAMESBbK8067.jpg" alt=""/>
+                                    <div>12</div>
+                                    {/*<div>*/}
+                                        {/*<div className="falls-cell">*/}
+                                            {/*<div className="falls-cell__image">*/}
+                                                {/*<img src="https://image.juooo.com/group1/M00/03/25/rAoKmV2f3-yADPouAACAMESBbK8067.jpg" alt=""/>*/}
+                                                {/*<span className="falls-cell__image__label">深圳</span>*/}
+                                            {/*</div>*/}
+                                        {/*</div>*/}
+                                    {/*</div>*/}
                                 </div>
                             </div>
-                            {/*<div  className="type-filtrate type-filtrate" >*/}
-                            {/*<section  className="dialog_bg">*/}
-                            {/*<div  className="filtrate_bg"></div>*/}
-                            {/*<div  className="filtrate-data">*/}
-                            {/*<div  className="city-list">*/}
-                            {/*<h3  className="title">*/}
-                            {/*<span className="name">城市</span>*/}
-                            {/*<span className="arr-icon"></span>*/}
-
-                            {/*</h3>*/}
-                            {/*<div className="list-wrap">*/}
-                            {/*<ul  className="list">*/}
-                            {/*<li className="active">全国</li>*/}
-                            {/*<li className>深圳</li>*/}
-                            {/*<li className>北京</li>*/}
-                            {/*</ul>*/}
-                            {/*</div>*/}
-                            {/*</div>*/}
-                            {/*<div className="foot">*/}
-                            {/*<span>重置</span>*/}
-                            {/*<span>确定</span>*/}
-                            {/*</div>*/}
-                            {/*</div>*/}
-                            {/*</section>*/}
-                            {/*</div>*/}
                         </div>
                     </div>
                 </div>
