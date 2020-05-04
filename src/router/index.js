7864779807909d0783a2fd94f962bd4f1f33b4d1
chuanchuan <1181977466@qq.com>
2020-05-05 00:22:05 +0800

    完成首页大部分功能

--- src/router/index.js
+++ src/router/index.js
@@ -24,7 +24,7 @@ export default [
     component: ThreaterDetail,
   },
   {
-    path: "/ticket",
+    path: "/ticket/:id",
     component: Ticket,
   },
   {
--- src/store/reducer/index.js
+++ src/store/reducer/index.js
@@ -1,8 +1,10 @@
 import { ticketReducer } from "./ticketReducer/index"
+import {homeReducer} from "./homeReducer/index"
 import dialog from "./Dialog/index"
 import { combineReducers } from "redux"
 const rootReducer = combineReducers({
   ticketReducer,
   dialog,
+  homeReducer
 })
 export default rootReducer
